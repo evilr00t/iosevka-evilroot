e3ab95f41171782d98a5061f4408a815fdb35fe1
Karol Czeryna <k@e-dot.uk>
2019-08-28 12:04:10 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+# check if we have ttfautohint
+if ! which ttfautohint >/dev/null 2>&1;
+then
+brew install ttfautohint
+fi
+# check if we have otfcc
+if ! which otfccdump >/dev/null 2>&1;
+then
+brew tap caryll/tap
+brew install otfcc-mac64
+fi
+
+if [ ! -d iosevka-repo ]; then
+  git clone https://github.com/be5invis/Iosevka.git iosevka-repo --depth=1
+  ln -s ${PWD}/private-build-plans.toml iosevka-repo/private-build-plans.toml
+else
+  pushd iosevka-repo
+  git pull --rebase
+  npm install
+  npm run build -- contents::iosevka-evilroot
+fi
