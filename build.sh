7fc2b3be71f0b16370a28cb7229efd396bae5bb8
Karol Czeryna <k@e-dot.uk>
2020-03-05 14:20:50 +0000

    Changes and fix build.sh

--- build.sh
+++ build.sh
@@ -13,13 +13,18 @@ brew tap caryll/tap
 brew install otfcc-mac64
 fi
 
+if ! which otf2otc >/dev/null 2>&1;
+then
+    pip3 install afdko
+fi
+
 if [ ! -d iosevka-repo ]; then
   git clone https://github.com/be5invis/Iosevka.git iosevka-repo --depth=1
-t  ln -s ${PWD}/private-build-plans.toml iosevka-repo/private-build-plans.toml
+  ln -s ${PWD}/private-build-plans.toml iosevka-repo/private-build-plans.toml
 fi
 
 pushd iosevka-repo
 git pull --rebase
 npm install
-npm run build -- contents::iosevka-evilroot
+npm run build -- ttf::iosevka-evilroot
 
